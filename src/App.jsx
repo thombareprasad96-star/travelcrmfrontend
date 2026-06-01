1641116dd0f22e2a1f1febf460bdfa204823b116
Man Singh <patelmansingh202@gmail.com>
2026-06-01 15:33:10 +0530

    up

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import AllLeads from "./admin/leads/AllLeads";
 import AdminLogin from "./login/AdminLogin";
 
 const App = () => {
-  // Check karega ki admin login hai ya nahi. Shuru me false rahega.
+ 
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   return (
     <BrowserRouter>
       <Routes>
         
-        {/* 1. LOGIN PAGE (Isko Layout se bahar rakha hai taaki Sidebar na dikhe) */}
+      
         <Route 
           path="/login" 
           element={
@@ -24,15 +24,15 @@ const App = () => {
           } 
         />
 
-        {/* 2. PROTECTED PAGES (Layout ke andar) */}
+      
         <Route 
           path="/" 
           element={
-            // Agar login nahi hai, toh wapas /login par bhej do
+           
             isAuthenticated ? <Layout /> : <Navigate to="/login" replace />
           }
         >
-          {/* Default route: Agar koi sirf '/' open kare toh allleads par chala jaye */}
+        
           <Route index element={<Navigate to="/" replace />} />
           
           <Route path="allleads" element={<AllLeads />} />
